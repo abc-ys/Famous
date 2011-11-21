afd9ba16d2d5d3843970fd2ed2c7327c4f8e7ad2
Lucy <Lucy@192.168.1.3>
2011-11-22 01:18:45 +0800

    更新商品管理

--- beFamous/WebContent/WEB-INF/view/manageProduct/queryProductData.jsp
+++ beFamous/WebContent/WEB-INF/view/manageProduct/queryProductData.jsp
@@ -10,57 +10,61 @@
 </head>
 <body>
 <form name="fm" method="post">
+	<p>
+	查詢商品資料
+	<br>
+	<font size="2">&nbsp商品類別:</font>&nbsp;
+	<select name="productionClassificationId">
+			<c:forEach var="hm" items="${productionClassification[0]}">
+				<option value="${hm.id}">${hm.name}</option> 
+			</c:forEach>
+		</select>
+	&nbsp;&nbsp;
+	<input type="submit" value="查詢" onclick="query()"/>
+	<p>
+	批次調整商品資料
+	<br>
+	<font size="2">&nbsp;調整比率:</font>&nbsp;
+	<select name="condition"> 
+	<option value="1">定價</option>
+	<option value="2">贈送Bonus</option>  
+	</select>&nbsp;&nbsp;&nbsp;售價x&nbsp
+	<input type="text" size="5" name="rate">&nbsp;&nbsp;&nbsp;
+	<input type="submit" value="調整" onclick="change()"/>
+</form>
 <p>
-查詢商品資料
-<br>
-<font size="2">&nbsp商品類別:</font>&nbsp
-<select name="productCategory"> 
-<option value=""></option> 
-<option value="4G GSiSD卡">4G GSiSD卡</option> 
-<option value="儲值 300元">儲值 300元</option> 
-</select>&nbsp&nbsp
-<input type="submit" value="查詢" onclick="query()"/>
-<p>
-批次調整商品資料
-<br>
-<font size="2">&nbsp調整比率:</font>&nbsp
-<select name="percentage"> 
-<option value=""></option> 
-<option value="調整特惠價">調整特惠價</option> 
-</select>&nbsp&nbsp&nbsp售價x&nbsp
-<input type="text" size="5" name="changePrice">&nbsp&nbsp&nbsp
-<input type="submit" value="調整" onclick="change()"/>
-<p>
-查詢結果&nbsp&nbsp&nbsp<input type="submit" value="儲存修改" onclick="saveModify()"/>
+
+<form name="fm2" method="post">
+查詢結果&nbsp;&nbsp;&nbsp;
 <table border="1" BorderColor="#000000" cellpadding="0" cellspacing="0">
-<tr><td Width="80" Height="35"><font size="2">商品編號</font></td>
-	<td Width="100"><font size="2">商品名稱</font></td>
-	<td Width="100"><font size="2">商品類別</font></td>
-	<td Width="100"><font size="2">廠商</font></td>
-	<td Width="40"><font size="2">上架</font></td>
-	<td Width="40"><font size="2">數量</font></td>
-	<td Width="50"><font size="2">售價</font></td>
-	<td Width="50"><font size="2">特惠價</font></td>
-	<td Width="80"><font size="2">Bonus購買額</font></td>
-	<td Width="100"><font size="2">贈送Bonus</font></td>
-	<td Width="40"><font size="2">編輯</font></td></tr>
+<tr><td Width="70" Height="35"><center><font size="2">商品編號</font></center></td>
+	<td Width="100"><center><font size="2">商品名稱</font></center></td>
+	<td Width="80"><center><font size="2">商品類別</font></center></td>
+	<td Width="80"><center><font size="2">廠商</font></center></td>
+	<td Width="40"><center><font size="2">上架</font></center></td>
+	<td Width="40"><center><font size="2">數量</font></center></td>
+	<td Width="50"><center><font size="2">售價</font></center></td>
+	<td Width="50"><center><font size="2">特惠價</font></center></td>
+	<td Width="100"><center><font size="2">Bonus購買額</font></center></td>
+	<td Width="80"><center><font size="2">贈送Bonus</font></center></td>
+	<td Width="50"><center><font size="2">編輯</font></center></td></tr>
 	
-	<c:forEach var="hm" items="${productionCategory}">
-<tr><td><font size="2">${hm.productionCategoryRid}</font></td>
-	<td><font size="2">${hm.productionCategoryName}</font></td>
-	<td><font size="2">${hm.productionCategoryName}</font></td>
-	<td><font size="2">${hm.sdCard.companyName}</font></td>
-	<td><INPUT type="checkbox" name="onsale"></td>
-	<td><font size="2">${hm.sdCard.amount}</font></td>
-	<td><input type=text name="price" size="3"></td>
-	<td><input type=text name="price2" size="3"></td>
-	<td><input type=text name="price3" size="6"></td>
-	<td><font size="2">${hm.sdCard.reward}</font></td>
-	<td><font size="2"><a href="javascript:editData()">編輯</a></font></td>
+	<c:forEach var="hm2" items="${productionClassification[1]}">
+	<tr><td><center><font size="2">${hm2.id}</font></center></td>
+		<td><center><font size="2">${hm2.name}</font></center></td>
+		<td><center><font size="2">${hm2.productionClassification.name}</font></center></td>
+		<td><center><font size="2">${hm2.companyName}</font></center></td>
+		<td><center><INPUT type="checkbox" name="status"></center></td>
+		<td><center><font size="2">${hm2.amount}</font></center></td>
+		<td><center><input type=text name="realPrice" size="3" value=${hm2.sdCardPrice.price}></center></td>
+		<td><center><input type=text name="specialPrice" size="3" value=${hm2.sdCardPrice.specialPrice}></center></td>
+		<td><center><font size="2">${hm2.sdCardPrice.discountPrice}元+&nbsp;${hm2.sdCardPrice.discountBonus}點</font></center></td>
+		<td><center><font size="2">${hm2.reward}</font></center></td>
+		<td><center><font size="2"><a href="javascript:editData(${hm2.id})">編輯</a></font></center></td>
 	</tr>
 	</c:forEach>
 </table>
-<input type="submit" value="儲存修改" onclick="change()"/>
+<input type="submit" value="儲存修改" onclick="saveModify()"/>
 </form>
 </body>
 <script>
@@ -72,13 +76,13 @@ function change(){
     document.fm.action="${pageContext.request.contextPath}/allChange.do";
     document.fm.submit();
 }
-function editData(){
-    document.fm.action="${pageContext.request.contextPath}/productDetailData.do";
-    document.fm.submit();
+function editData(productId){
+    document.fm2.action="${pageContext.request.contextPath}/productDetailData.do?productId="+productId;
+    document.fm2.submit();
 }
-function saveModify(){
-    document.fm.action="${pageContext.request.contextPath}/saveModify.do";
-    document.fm.submit();
+function saveModify(){ 
+    document.fm2.action="${pageContext.request.contextPath}/saveModify.do";
+    document.fm2.submit();
 }
 </script>
 </html>
--- beFamous/src/com/ubn/befamous/service/TransactionRecordService.java
+++ beFamous/src/com/ubn/befamous/service/TransactionRecordService.java
@@ -24,82 +24,84 @@ public interface TransactionRecordService{
 	//SD卡與儲值service
 	public SDCard[] querySDCard();
 	public PrePaid[] queryPrePaid();
-	public ArrayList addShoppingCart(Long userId, Long productId, String amount);
-	public ArrayList purchaseConfirmShoppingCart(Long userId, ShoppingCart shoppingCart);
-	public Order addOrder(Long shoppingCartId);
+	public ArrayList addShoppingCart(long userId, long productId, String amount);
+	public ArrayList purchaseConfirmShoppingCart(long userId, ShoppingCart shoppingCart);
+	public Order addOrder(long shoppingCartId);
 	
 	//購物車service
-	public ShoppingCartDetail[] queryShoppingCart(Long userId);
-	public ShoppingCartDetail[] deleteShoppingCart(Long userId,Long transactionId);
-	public ShoppingCartDetail[] forwardPurchase(Long userId);
-	public void purchaseConfirm(Long shoppingCartId, String gsiMoney, String gsiBonus);
+	public ShoppingCartDetail[] queryShoppingCart(long userId);
+	public ShoppingCartDetail[] deleteShoppingCart(long userId,long transactionId);
+	public ShoppingCartDetail[] forwardPurchase(long userId);
+	public void purchaseConfirm(long shoppingCartId, String gsiMoney, String gsiBonus);
 	
 	//訂單管理service-實體幣訂單
-	public Order[] queryCashTransRcd(String userEmail, Long transactionId, String startDate, String endDate, String orderStatus);
-	public ArrayList queryCashTransDetail(Long transactionId);
-	public boolean saveCashTransRcd(Long transactionId,String billStatus, String payStatus, String processStatus, String memo1, String memo2);
-	public Order queryDeliveryRcd(Long transactionId);
-	public ArrayList updateDeliveryRcd(Long transactionId, String deliveryDate, String deliveryNo, String billNo);
-	public ArrayList returnGoods(Long transactionId);
-	public ArrayList cancelOrder(Long transactionId);
+	public Order[] queryCashTransRcd(String userEmail, long transactionId, String startDate, String endDate, String orderStatus);
+	public ArrayList queryCashTransDetail(long transactionId);
+	public boolean saveCashTransRcd(long transactionId,String billStatus, String payStatus, String processStatus, String memo1, String memo2);
+	public Order queryDeliveryRcd(long transactionId);
+	public ArrayList updateDeliveryRcd(long transactionId, String deliveryDate, String deliveryNo, String billNo);
+	public ArrayList returnGoods(long transactionId);
+	public ArrayList cancelOrder(long transactionId);
 	
 	//訂單管理service-虛擬幣訂單
-	public Order[] queryInventTransRcd(String userEmail, Long transactionId, String startDate, String endDate, String orderStatus);
-	public OrderDetail queryInventTransDetail(Long transactionId);
-	public Order queryInventMemo(Long transactionId);
-	public boolean updateInventMemo(Long transactionId, String memo1, String memo2);
+	public Order[] queryInventTransRcd(String userEmail, long transactionId, String startDate, String endDate, String orderStatus);
+	public OrderDetail queryInventTransDetail(long transactionId);
+	public Order queryInventMemo(long transactionId);
+	public boolean updateInventMemo(long transactionId, String memo1, String memo2);
 
 	//發票登錄-兌換金額管理
 	public GsiMoney[] queryExchangeList(String year, String month);
-	public GsiMoney queryExchangeDetail(Long gsiMoneyId);
+	public GsiMoney queryExchangeDetail(long gsiMoneyId);
 	public GsiMoney[] updateExchange(GsiMoney gsiMoney, String year, String month);
 	
 	//發票登錄-訂單發票登錄
 	public Order[] queryBillRcd(String year, String month);
-	public OrderDetail queryBillDetail(Long transactionId);
+	public OrderDetail queryBillDetail(long transactionId);
 	public Order[] updateBill(Order order, String year, String month);
 
 	//GSiMoney&GSiBonus
-	public GsiMoney queryGsiMoney(Long userId);
-	public ArrayList queryTransRcd(Long userId, String year, String month);
-	public OrderDetail[] queryTransDetail(Long transactionId);
-	public ArrayList applyChange(Long userId);
+	public GsiMoney queryGsiMoney(long userId);
+	public ArrayList queryTransRcd(long userId, String year, String month);
+	public OrderDetail[] queryTransDetail(long transactionId);
+	public ArrayList applyChange(long userId);
 	public boolean Savechange(Member member, String money);
-	public GsiMoney[] queryExchangeLog(Long userId, String year, String month);
-	public Order[] queryPrePaidRcd(Long userId, String year, String month);
-	public Order[] queryBuyLogRcd(Long userId, String year, String month);
-	public OrderDetail[] queryBuyLogDetail(Long transactionId);
-	public ArrayList queryBonusDetail(Long userId);
-	public Order[] queryCashRcd(Long userId,String status, Long transactionId);
-	public OrderDetail[] queryCashRcdDetail(Long transactionId);
+	public GsiMoney[] queryExchangeLog(long userId, String year, String month);
+	public Order[] queryPrePaidRcd(long userId, String year, String month);
+	public Order[] queryBuyLogRcd(long userId, String year, String month);
+	public OrderDetail[] queryBuyLogDetail(long transactionId);
+	public ArrayList queryBonusDetail(long userId);
+	public Order[] queryCashRcd(long userId,String status, long transactionId);
+	public OrderDetail[] queryCashRcdDetail(long transactionId);
 	public void downloadReturnList();
 	
 	//檢舉管理
-	public ArrayList queryOffenseCategory(Long productionCategoryId);
-	public void addOffense(Long userId, Long productionCategoryId, long offenseTypeId, String reason);
+	public ArrayList queryOffenseCategory(long productionCategoryId);
+	public void addOffense(long userId, long productionCategoryId, long offenseTypeId, String reason);
 	public ArrayList queryUnHandle();
-	public ArrayList updateAlbumHide(Long adminId, Long albumId);
-	public ArrayList updateSongHide(Long adminId, Long songId);
-	public Offense[] queryOffenseReason(Long productionCategoryId);
+	public ArrayList updateAlbumHide(long adminId, long albumId);
+	public ArrayList updateSongHide(long adminId, long songId);
+	public Offense[] queryOffenseReason(long productionCategoryId);
 	public Album[] queryAutoHide();
 	public ArrayList queryAutoHideByDate(String type,String year, String month, String creator);
-	public void canceAlbumHide(Long adminId, Long albumId);
-	public void canceSonglHide(Long adminId, Long songId);
+	public void canceAlbumHide(long adminId, long albumId);
+	public void canceSonglHide(long adminId, long songId);
 	public Album[] queryAlreadyHide(String type);
 	public ArrayList queryAlreadyHideByDate(String year, String month, String creator);
-	public Offense[] queryOffenseByUser(Long userId);
-	public void updateIncorrectOffense(Long adminId, Long offenseId);
+	public Offense[] queryOffenseByUser(long userId);
+	public void updateIncorrectOffense(long adminId, long offenseId);
 	
 	
 	//商品管理
 	public void addProduct(File file);
 	public ProductionClassification[] queryProductionClassification();
 	public void addProductionClassification(String productionClassificationName);
-	public void editProductionClassification(Long productionClassificationId, String productionClassificationName);
-	public ProductionClassification[] deleteProductionClassification(Long roductionClassificationId);
-	public ArrayList queryProductInfo(Long roductionClassificationId);
-	public ArrayList updateProductBatch(Long productionClassificationId, String condition, double rate);
-	public ProductionClassification queryProductDetail(Long productId);
-	public ProductionClassification[] updateProduct(Long productId, String productName,String roductionClassification, String realPrice,String specialPrice, String giveBonus, String stock, String status, String keyword, String memo);
-
+	public void editProductionClassification(long productionClassificationId, String productionClassificationName);
+	public ProductionClassification[] deleteProductionClassification(long roductionClassificationId);
+	public ArrayList queryProduct();
+	public ArrayList queryProductInfo(long roductionClassificationId);
+	public void updateProductBatch(long productionClassificationId, String condition, double rate);
+	public ArrayList queryProductDetail(long productId);
+	public long saveModify(long productId, String realPrice,String specialPrice, String status, long productionClassificationId);
+	public long updateProduct(long productId, String productName,long newProductionClassificationId, String transactionType,String realPrice,String specialPrice, String discountPrice, String discountBonus, String giveBonus, String stock, String status, String keyword, String memo, long productionClassificationId);
+	
 }
