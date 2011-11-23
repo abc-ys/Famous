4b8ba816f96a8e42d432a2d37d17eedf7c5e1883
YSChen <es200283@msn.com>
2011-11-23 11:50:16 +0800

    修改線上客服和上傳音樂的controller

--- beFamous/src/com/ubn/befamous/controller/OnlineQuestionController.java
+++ beFamous/src/com/ubn/befamous/controller/OnlineQuestionController.java
@@ -1,13 +1,25 @@
 package com.ubn.befamous.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ubn.befamous.dao.IBaseDao;
+import com.ubn.befamous.entity.Admin;
+import com.ubn.befamous.entity.Question;
+import com.ubn.befamous.service.MusicService;
+import com.ubn.befamous.service.PersonService;
+
 @Controller
 @SessionAttributes
 public class OnlineQuestionController {
+	
+	@Autowired
+	 PersonService personService;
 	
 	@RequestMapping("/addQuestion")
 	public ModelAndView addQuestion()
@@ -23,41 +35,18 @@ public class OnlineQuestionController {
 		System.out.println("saveQuestion==>");
 		System.out.println("    productionType="+productionType+", userIdentity="+userIdentity+", userName="+userName+", email="+email+", tel="+tel+", questionType="+questionType+", questionContent="+questionContent);
 		String questionDate = "2011/11/01";
+		
+		personService.saveQuestion(productionType, userIdentity, userName, email, tel, questionType, questionContent);
 		return new ModelAndView("addQuestion");
 	}
 	
 	//查詢問題清單 (base on 問題管理連結)
 		@RequestMapping("/manageQuestion")
-		public ModelAndView manageQuestion(Model model)
+		public ModelAndView manageQuestion(Model model,String adminId)
 		{
 			System.out.println("manageQuestion==>");
 			
-			String adminId="9999";
-				
-			Question q1 = new Question();
-			q1.setQuestionId(111);
-			q1.setQuestionType("下載問題");
-			q1.setQuestionDate("2011-11-11 11-11-11");
-			q1.setProductionType("PC");
-			q1.setUserIdentity("會員");
-			q1.setUserName("王大明");
-			q1.setEmail("aaa@gmail.com");
-			q1.setTel("0911234567");
-			q1.setHandleStatus("未回覆");
-			q1.setNoteContent("qqq");
-				
-			Question q2 = new Question();
-			q2.setQuestionId(111);
-			q2.setQuestionType("軟體相關");
-			q2.setQuestionDate("2011-11-11 11-11-11");
-			q2.setProductionType("Android");
-			q2.setUserIdentity("非會員");
-			q2.setUserName("李曉明");
-			q2.setEmail("bbb@gmail.com");
-			q2.setTel("0922234567");
-			q2.setHandleStatus("未回覆");
-				
-			Question[] questionList = {q1,q2};
+			Question[] questionList = personService.queryQuestion("", "", "", "", "");
 			model.addAttribute("admin", adminId);
 			model.addAttribute("questionList", questionList);
 			return new ModelAndView("manageQuestion");
@@ -65,37 +54,13 @@ public class OnlineQuestionController {
 		
 		//查詢問題清單 (base on 查詢條件)
 		@RequestMapping("/queryQuestionList")
-		public ModelAndView queryQuestionList(String adminId, String startDate,String endDate, String productionType, String questionType, Model model)
+		public ModelAndView queryQuestionList(String adminId, String startDate,String endDate, String productionType, String questionType, String email, Model model)
 		{
 			System.out.println("queryQuestionList==>");
 			System.out.println("	startDate="+startDate+", endDate="+endDate+", productionType="+productionType+", questionType="+questionType);
 			
-			adminId="9999";
 			
-			Question q1 = new Question();
-			q1.setQuestionId(111);
-			q1.setQuestionType("下載問題");
-			q1.setQuestionDate("2011-11-11 11-11-11");
-			q1.setProductionType("PC");
-			q1.setUserIdentity("會員");
-			q1.setUserName("王大明");
-			q1.setEmail("aaa@gmail.com");
-			q1.setTel("0911234567");
-			q1.setHandleStatus("未回覆");
-			q1.setNoteContent("qqq");
-			
-			Question q2 = new Question();
-			q2.setQuestionId(111);
-			q2.setQuestionType("軟體相關");
-			q2.setQuestionDate("2011-11-11 11-11-11");
-			q2.setProductionType("Android");
-			q2.setUserIdentity("非會員");
-			q2.setUserName("李曉明");
-			q2.setEmail("bbb@gmail.com");
-			q2.setTel("0922234567");
-			q2.setHandleStatus("未回覆");
-			
-			Question[] questionList = {q1,q2};
+			Question[] questionList = personService.queryQuestion(startDate, endDate, productionType, email, questionType);
 			model.addAttribute("admin", adminId);
 			model.addAttribute("questionList", questionList);
 			return new ModelAndView("queryQuestionList");
@@ -107,18 +72,10 @@ public class OnlineQuestionController {
 			System.out.println("queryQuestionDetail==>");
 			System.out.println("	qId="+qId);
 			
-			Question q1 = new Question();
-			q1.setQuestionId(111);
-			q1.setQuestionContent("XXXXXXXXXXXXXXXXXXXXXXXXXX");
-			q1.setAnswerContent("YYYYYYYYYYYYYYYYYYYYYYYYYYYYYY");
-			q1.setAnswerDate("2011-11-12 12:12:12");
-			q1.setAnswerPerson("管理者一號");
-			q1.setNoteContent("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ");
-			q1.setNoteDate("2011-11-13 13:13:13");
-			q1.setNotePerson("管理者二號");
+			Question q = personService.queryQuestionDetail(Long.parseLong(qId));
 			
 			model.addAttribute("admin", adminId);
-			model.addAttribute("questionDetail", q1);
+			model.addAttribute("questionDetail", q);
 			return new ModelAndView("queryQuestionDetail");
 		}
 		//管理者回覆問題 (window open)
@@ -127,21 +84,21 @@ public class OnlineQuestionController {
 			System.out.println("addAnswer==>");		
 			System.out.println("    qId="+qId+", adminId="+adminId);
 			
-			Admin admin = new Admin();
-			admin.setAdminName("BBB");
-			admin.setAdminId(9999);
+			Admin admin = personService.queryAdminName(Long.parseLong(adminId));
 			
 			model.addAttribute("qId",qId);
 			model.addAttribute("admin",admin);
+			model.addAttribute("adminId",adminId);
 			return new ModelAndView("addAnswer");
 		}
 			
 		//儲存回覆內容
 		@RequestMapping("/saveAnswer")
-		public ModelAndView saveAnswer(String adminId,String qId,String answer, Model model) {						
+		public ModelAndView saveAnswer(String adminId,String qId,String answer,String adminName, Model model) {						
 			System.out.println("saveAnswer==>");		
 			System.out.println("    qId="+qId+", adminId="+adminId+", answer="+answer);
 			
+			personService.saveAnswer(Long.valueOf(qId), Long.valueOf(adminId), answer, adminName);
 			
 			return new ModelAndView("saveAnswer");
 		}
@@ -152,20 +109,22 @@ public class OnlineQuestionController {
 			System.out.println("addNote==>");		
 			System.out.println("    qId="+qId+", adminId="+adminId);
 			
-			Admin admin = new Admin();
-			admin.setAdminName("BBB");
-			admin.setAdminId(9999);
+			Admin admin = personService.queryAdminName(Long.parseLong(adminId));
 			
 			model.addAttribute("qId",qId);
 			model.addAttribute("admin",admin);
+			model.addAttribute("adminId",adminId);
 			return new ModelAndView("addNote");
 		}
 				
 		//儲存備註
 		@RequestMapping("/saveNote")
-		public ModelAndView saveNote(String adminId,String qId,String note, Model model) {						
+		public ModelAndView saveNote(String adminId,String qId,String note,String adminName, Model model) {						
 			System.out.println("saveNote==>");		
-			System.out.println("    qId="+qId+", adminId="+adminId+", note="+note);
+			System.out.println("    qId="+qId+", adminId="+adminId+", note="+note+", adminName="+adminName);
+			
+			personService.saveNote(Long.valueOf(qId), Long.valueOf(adminId), note, adminName);
+			
 			return new ModelAndView("saveNote");
 		}
 		
